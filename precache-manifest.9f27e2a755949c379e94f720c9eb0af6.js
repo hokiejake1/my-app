06cba0ebe74255b0df1dd0ca5e3f3006cc430c7d
Jake Huffman <hokiejake1@gmail.com>
2024-01-07 18:06:06 -0500

    Updates

---
+++ precache-manifest.9f27e2a755949c379e94f720c9eb0af6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94bc9d5ed2ed7b317e729421be79a5bd",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "5982d34947dda9b77212",
+    "url": "/my-app/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "1f5929bab9e74fc2d234",
+    "url": "/my-app/static/js/2.a71b1702.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/my-app/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "5982d34947dda9b77212",
+    "url": "/my-app/static/js/main.c52684ed.chunk.js"
+  },
+  {
+    "revision": "963c8cf830fccb149c29",
+    "url": "/my-app/static/js/runtime~main.e92001b1.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/my-app/static/media/logo.06e73328.svg"
+  }
+]);
